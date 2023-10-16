52c23e923153be4945ce4cd76e505c866ac7f07c
Yash <Yash@gmail.com>
2023-10-16 17:41:27 +0530

    Prediction Pipelines

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,10 @@
 import os
 import sys
-
+import pickle
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
+
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -17,7 +17,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -56,7 +56,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
